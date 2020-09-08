bf63d56e97efafaee932847f244d3f54c7512181
Yamil <yamil.foglia@outlook.com>
2020-09-08 20:59:38 -0300

    web online

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { CustomersComponent } from './components/views/customers/customers.component';
+import { InfoComponent } from './components/views/info/info.component';
+import { FooterComponent } from './components/feature/shared/footer/footer.component';
+import { NavbarComponent } from './components/feature/shared/navbar/navbar.component';
+import { ContentCardsComponent } from './components/feature/shared/content-cards/content-cards.component';
+import { QuestionFormComponent } from './components/feature/shared/question-form/question-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CustomersComponent,
+    InfoComponent,
+    FooterComponent,
+    NavbarComponent,
+    ContentCardsComponent,
+    QuestionFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/feature/shared/content-cards/content-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-cards',
+  templateUrl: './content-cards.component.html',
+  styleUrls: ['./content-cards.component.scss']
+})
+export class ContentCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
